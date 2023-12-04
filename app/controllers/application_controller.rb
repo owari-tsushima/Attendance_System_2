69deae776a542d8c58ec9c252f2520918e4b31b0
maekawa <kailas8jp@gmail.com>
2023-12-04 01:51:11 +0000

    デプロイ後のセキュリティを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,4 +54,13 @@ class ApplicationController < ActionController::Base
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
     redirect_to root_url
   end
+  
+  # 管理権限者、または現在ログインしているユーザーを許可します。
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end  
+    end
 end
